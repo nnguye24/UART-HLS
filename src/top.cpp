d802cfb8d4e4ed15ddfff419be4041158d42d119
Nguyen Nguyen <nnguye24@nd.edu>
2025-04-29 23:10:43 -0400

    updated top modules

--- src/top.cpp
+++ src/top.cpp
@@ -1,121 +1,189 @@
-#include "top.h"
+/*********************************************
+ * File name: top.cpp
+ * Authors: Luke Guenthner, Nguyen Nguyen, Marcellus Wilson
+ * Date: 4/29/2025
+ *
+ * This file contains the top module implementation for
+ * the UART controller
+ *********************************************/
 
-void top::process() {
-  // Initial reset
-  while (true) {
-    if (!rst.read()) {
-      HLS_DEFINE_PROTOCOL("reset");
-      
-      datapath_inst.reset();
-      controller_inst.reset();
-      memory_map_inst.reset();
-    } else {
-      compute();
-      commit();
-    }
+ #include "top.h"
 
-    {
-      HLS_DEFINE_PROTOCOL("wait");
-      wait(); // Next cycle
-    }
-  }
-}
-
-void top::compute() {
-  // === INPUT PHASE ===
-  // Read inputs from signals
-  bool in_rst = rst.read();
-  bool in_chip_select = chip_select.read();
-  bool in_read_write = read_write.read();
-  bool in_write_enable = write_enable.read();
-  bool in_rx_in = rx_in.read();
-  sc_uint<DATA_W> in_data_in = data_in.read();
-  sc_uint<ADDR_W> in_addr = addr.read();
-  
-  // === MEMORY MAP INPUTS ===
-  memory_map_inst.rst = in_rst;
-  memory_map_inst.data_in = in_data_in;
-  memory_map_inst.addr = in_addr;
-  memory_map_inst.chip_select = in_chip_select;
-  memory_map_inst.read_write = in_read_write;
-  memory_map_inst.write_enable = in_write_enable;
-  
-  // Connect status signals from datapath to memory map
-  memory_map_inst.tx_buffer_full = datapath_inst.tx_buffer_full;
-  memory_map_inst.rx_buffer_empty = datapath_inst.rx_buffer_empty;
-  memory_map_inst.error_indicator = datapath_inst.parity_error || 
-                                   datapath_inst.framing_error || 
-                                   datapath_inst.overrun_error;
-                                   
-  // Connect datapath signals to memory map
-  memory_map_inst.dp_data_in = datapath_inst.dp_data_in;
-  memory_map_inst.dp_addr = datapath_inst.dp_addr;
-  memory_map_inst.dp_write_enable = datapath_inst.dp_write_enable;
-  
-  // Compute memory map
-  memory_map_inst.compute();
-  
-  // === CONTROLLER INPUTS ===
-  controller_inst.clk = clk.read();
-  controller_inst.rst = in_rst;
-  controller_inst.tx_buffer_full = datapath_inst.tx_buffer_full;
-  controller_inst.rx_buffer_empty = datapath_inst.rx_buffer_empty;
-  controller_inst.rx_in = in_rx_in;
-  controller_inst.parity_error = datapath_inst.parity_error;
-  controller_inst.framing_error = datapath_inst.framing_error;
-  controller_inst.overrun_error = datapath_inst.overrun_error;
-  
-  // Get configuration from datapath
-  controller_inst.parity_enabled = datapath_inst.ctrl_parity_enabled;
-  controller_inst.parity_even = datapath_inst.ctrl_parity_even;
-  controller_inst.data_bits = datapath_inst.ctrl_data_bits;
-  controller_inst.stop_bits = datapath_inst.ctrl_stop_bits;
-  
-  // Compute controller FSM
-  controller_inst.compute();
-  
-  // === DATAPATH INPUTS ===
-  datapath_inst.clk = clk.read();
-  datapath_inst.rst = in_rst;
-  
-  // Control signals from controller
-  datapath_inst.load_tx = controller_inst.out_load_tx;
-  datapath_inst.load_tx2 = controller_inst.out_load_tx2; // Added missing load_tx2 signal
-  datapath_inst.tx_start = controller_inst.out_tx_start;
-  datapath_inst.tx_data = controller_inst.out_tx_data;
-  datapath_inst.tx_parity = controller_inst.out_tx_parity;
-  datapath_inst.tx_stop = controller_inst.out_tx_stop;
-  datapath_inst.rx_start = controller_inst.out_rx_start;
-  datapath_inst.rx_data = controller_inst.out_rx_data;
-  datapath_inst.rx_parity = controller_inst.out_rx_parity;
-  datapath_inst.rx_stop = controller_inst.out_rx_stop;
-  datapath_inst.error_handle = controller_inst.out_error_handle;
-  datapath_inst.rx_read = controller_inst.out_rx_read;
-  
-  // Data from memory map
-  datapath_inst.data_in = memory_map_inst.data_out;
-  // RX input signal
-  datapath_inst.rx_in = in_rx_in;
-  
-  // Compute datapath
-  datapath_inst.compute();
-}
-
-void top::commit() {
-  // Commit state updates for all modules
-  datapath_inst.commit();
-  controller_inst.commit();
-  memory_map_inst.commit();
-  
-  // Synchronize configuration between datapath and controller
-  datapath_inst.sync_controller_config();
-  
-  // === OUTPUT PINS ===
-  data_out.write(memory_map_inst.data_out);
-  tx_out.write(datapath_inst.tx_out);
-  tx_buffer_full.write(datapath_inst.tx_buffer_full);
-  rx_buffer_empty.write(datapath_inst.rx_buffer_empty);
-  error_indicator.write(datapath_inst.parity_error || 
-                        datapath_inst.framing_error || 
-                        datapath_inst.overrun_error);
-}
+ void top::process() {
+   // Initial reset
+   {
+     HLS_DEFINE_PROTOCOL("reset");
+     
+     // Each module has its own reset method
+     // that will be called through the signal connections
+   }
+   
+   {       
+     HLS_DEFINE_PROTOCOL("wait");
+     wait();
+   }
+ 
+   // Main processing loop
+   while(true) {
+     // Read inputs first
+     {
+       HLS_DEFINE_PROTOCOL("input");
+       
+       // We will come back here
+       read_inputs();
+     }
+     
+     // Processing phase
+     {
+       HLS_DEFINE_PROTOCOL("datapath_ops");
+       
+       // Allow modules to process
+       // They will read from their input ports
+       // The modules' process methods are triggered by clock edges
+     }
+     
+     // Output phase
+     {
+       HLS_DEFINE_PROTOCOL("output");
+       
+       write_outputs();
+     }
+     
+     // Wait for next cycle
+     {       
+       HLS_DEFINE_PROTOCOL("wait");
+       wait();
+     }
+     
+     // Additional wait for processing time
+     {       
+       HLS_DEFINE_PROTOCOL("wait");
+       wait();
+     }
+   }
+ }
+ 
+ void top::read_inputs() {
+   // Read inputs from external pins to internal variables
+   in_rst = rst.read();
+   in_data_in = data_in.read();
+   in_addr = addr.read();
+   in_chip_select = chip_select.read();
+   in_read_write = read_write.read();
+   in_write_enable = write_enable.read();
+   in_rx_in = rx_in.read();
+   
+   // Set internal control signals
+   start_signal.write(false);  // No external start in this implementation
+   mem_we_signal.write(in_write_enable);
+ }
+ 
+ 
+ void top::write_outputs() {
+   // Write external outputs
+   data_out.write(memory_map_inst.data_out);
+   tx_out.write(datapath_inst.tx_out);
+   tx_buffer_full.write(datapath_inst.tx_buffer_full);
+   rx_buffer_empty.write(datapath_inst.rx_buffer_empty);
+   error_indicator.write(datapath_inst.parity_error || 
+                         datapath_inst.framing_error || 
+                         datapath_inst.overrun_error);
+ }
+ 
+ 
+ bool top::test_reset_datapath() {
+   // Check if datapath registers are reset properly
+   bool tx_reg_ok = (datapath_inst.tx_shift_register == 0);
+   bool rx_reg_ok = (datapath_inst.rx_shift_register == 0);
+   bool buffers_ok = (datapath_inst.tx_buf_head == 0) &&
+                     (datapath_inst.tx_buf_tail == 0) &&
+                     (datapath_inst.rx_buf_head == 0) &&
+                     (datapath_inst.rx_buf_tail == 0);
+   bool counters_ok = (datapath_inst.tx_bit_count == 0) &&
+                      (datapath_inst.rx_bit_count == 0);
+   bool signals_ok = (datapath_inst.out_tx_out == 1) &&
+                     (datapath_inst.out_tx_buffer_full == false) &&
+                     (datapath_inst.out_rx_buffer_empty == true);
+   
+   if(!tx_reg_ok) {
+     std::cout << "datapath_inst.tx_shift_register - " << datapath_inst.tx_shift_register << std::endl;
+     return false;
+   }
+   
+   if(!rx_reg_ok) {
+     std::cout << "datapath_inst.rx_shift_register - " << datapath_inst.rx_shift_register << std::endl;
+     return false;
+   }
+   
+   if(!buffers_ok) {
+     std::cout << "Buffer pointers not reset properly" << std::endl;
+     return false;
+   }
+   
+   if(!counters_ok) {
+     std::cout << "Bit counters not reset properly" << std::endl;
+     return false;
+   }
+   
+   if(!signals_ok) {
+     std::cout << "Output signals not reset properly" << std::endl;
+     return false;
+   }
+   
+   return true;
+ }
+ 
+ bool top::test_reset_controller() {
+   // Check if controller state is properly reset
+   if(controller_inst.tx_state != TX_IDLE) {
+     std::cout << "controller_inst.tx_state - " << controller_inst.tx_state << std::endl;
+     return false;
+   }
+   
+   if(controller_inst.rx_state != RX_IDLE) {
+     std::cout << "controller_inst.rx_state - " << controller_inst.rx_state << std::endl;
+     return false;
+   }
+   
+   if(controller_inst.tx_next_state != TX_IDLE) {
+     std::cout << "controller_inst.tx_next_state - " << controller_inst.tx_next_state << std::endl;
+     return false;
+   }
+   
+   if(controller_inst.rx_next_state != RX_IDLE) {
+     std::cout << "controller_inst.rx_next_state - " << controller_inst.rx_next_state << std::endl;
+     return false;
+   }
+   
+   // Check all control signals are reset
+   if(controller_inst.out_load_tx || 
+      controller_inst.out_load_tx2 || 
+      controller_inst.out_tx_start || 
+      controller_inst.out_tx_data || 
+      controller_inst.out_tx_parity || 
+      controller_inst.out_tx_stop || 
+      controller_inst.out_rx_start ||
+      controller_inst.out_rx_data ||
+      controller_inst.out_rx_parity ||
+      controller_inst.out_rx_stop ||
+      controller_inst.out_rx_read ||
+      controller_inst.out_error_handle) {
+     
+     std::cout << "Controller outputs not reset properly" << std::endl;
+     return false;
+   }
+   
+   return true;
+ }
+ 
+ bool top::test_reset_memory_map() {
+   // Verify memory map is properly reset
+   for (int i = 0; i < RAM_SIZE; i++) {
+     if (memory_map_inst.Memory[i] != 0) {
+       std::cout << "Memory[" << i << "] is not reset, equals " << memory_map_inst.Memory[i] << std::endl;
+       return false;
+     }
+   }
+   
+   return true;
+ }
--- src/top.h
+++ src/top.h
@@ -1,70 +1,217 @@
-#ifndef __TOP_H__
-#define __TOP_H__
+/**************************************************************
+ * File Name: top.h
+ * Authors: Luke Guenthner, Nguyen Nguyen, Marcellus Wilson
+ * Date: 4/29/2025
+ *
+ * Contains the SystemC Module Header for the UART top-level module
+ **************************************************************/
 
-#include "systemc.h"
-#include "stratus_hls.h"
-#include "sizes.h"
-#include "datapath.h"
-#include "controller.h"
-#include "memory_map.h"
-
-SC_MODULE(top) {
-  // Inputs from testbench
-  sc_in<bool> clk;
-  sc_in<bool> rst;
-  sc_in<sc_uint<DATA_W>> data_in;
-  sc_in<sc_uint<ADDR_W>> addr;
-  sc_in<bool> chip_select;
-  sc_in<bool> read_write;
-  sc_in<bool> write_enable;
-  sc_in<bool> rx_in;
-
-  // Outputs to testbench
-  sc_out<sc_uint<DATA_W>> data_out;
-  sc_out<bool> tx_out;
-  sc_out<bool> tx_buffer_full;
-  sc_out<bool> rx_buffer_empty;
-  sc_out<bool> error_indicator;
-
-  // Submodules
-  datapath datapath_inst;
-  controller controller_inst;
-  memory_map memory_map_inst;
-
-  // Top-level thread
-  void process();
-  void compute();
-  void commit();
-
-  SC_CTOR(top) : datapath_inst(), controller_inst(), memory_map_inst() {
-    SC_THREAD(process);
-    sensitive << clk.pos();
-    async_reset_signal_is(rst, false);
-  }
-  
-#ifdef NC_SYSTEMC
- public:
-  void ncsc_replace_names(){
-    // Replace all the names using ncsc_replace_name for inputs and outputs
-    
-    // Inputs
-    ncsc_replace_name(clk, "clk");                      // Port 0
-    ncsc_replace_name(rst, "rst");                      // Port 1
-    ncsc_replace_name(data_in, "data_in");              // Port 2
-    ncsc_replace_name(addr, "addr");                    // Port 3
-    ncsc_replace_name(chip_select, "chip_select");      // Port 4
-    ncsc_replace_name(read_write, "read_write");        // Port 5
-    ncsc_replace_name(write_enable, "write_enable");    // Port 6
-    ncsc_replace_name(rx_in, "rx_in");                  // Port 7
-    
-    // Outputs
-    ncsc_replace_name(data_out, "data_out");            // Port 8
-    ncsc_replace_name(tx_out, "tx_out");                // Port 9
-    ncsc_replace_name(tx_buffer_full, "tx_buffer_full");// Port 10
-    ncsc_replace_name(rx_buffer_empty, "rx_buffer_empty");// Port 11
-    ncsc_replace_name(error_indicator, "error_indicator");// Port 12
-  }
-#endif
-};
-
-#endif
+ #ifndef __TOP_H__
+ #define __TOP_H__
+ 
+ #include "systemc.h"
+ #include "stratus_hls.h"
+ #include "sizes.h"
+ #include "datapath.h"
+ #include "controller.h"
+ #include "memory_map.h"
+ 
+ SC_MODULE(top) {
+   // Inputs from testbench
+   sc_in<bool> clk;                        // Port 0
+   sc_in<bool> rst;                        // Port 1
+   sc_in<sc_uint<DATA_W>> data_in;         // Port 2
+   sc_in<sc_uint<ADDR_W>> addr;            // Port 3
+   sc_in<bool> chip_select;                // Port 4
+   sc_in<bool> read_write;                 // Port 5
+   sc_in<bool> write_enable;               // Port 6
+   sc_in<bool> rx_in;                      // Port 7
+ 
+   // Outputs to testbench
+   sc_out<sc_uint<DATA_W>> data_out;       // Port 8
+   sc_out<bool> tx_out;                    // Port 9
+   sc_out<bool> tx_buffer_full;            // Port 10
+   sc_out<bool> rx_buffer_empty;           // Port 11
+   sc_out<bool> error_indicator;           // Port 12
+ 
+   // Submodules
+   datapath datapath_inst;
+   controller controller_inst;
+   memory_map memory_map_inst;
+ 
+   // Internal signals for connecting modules
+   
+   // Controller to datapath signals
+   sc_signal<bool> ctrl_to_dp_load_tx;
+   sc_signal<bool> ctrl_to_dp_load_tx2;
+   sc_signal<bool> ctrl_to_dp_tx_start;
+   sc_signal<bool> ctrl_to_dp_tx_data;
+   sc_signal<bool> ctrl_to_dp_tx_parity;
+   sc_signal<bool> ctrl_to_dp_tx_stop;
+   sc_signal<bool> ctrl_to_dp_rx_start;
+   sc_signal<bool> ctrl_to_dp_rx_data;
+   sc_signal<bool> ctrl_to_dp_rx_parity;
+   sc_signal<bool> ctrl_to_dp_rx_stop;
+   sc_signal<bool> ctrl_to_dp_error_handle;
+   sc_signal<bool> ctrl_to_dp_rx_read;
+   
+   // Datapath to controller signals
+   sc_signal<bool> dp_to_ctrl_tx_buffer_full;
+   sc_signal<bool> dp_to_ctrl_rx_buffer_empty;
+   sc_signal<bool> dp_to_ctrl_parity_error;
+   sc_signal<bool> dp_to_ctrl_framing_error;
+   sc_signal<bool> dp_to_ctrl_overrun_error;
+   
+   // Configuration signals
+   sc_signal<bool> dp_to_ctrl_parity_enabled;
+   sc_signal<bool> dp_to_ctrl_parity_even;
+   sc_signal<sc_uint<3>> dp_to_ctrl_data_bits;
+   sc_signal<sc_uint<2>> dp_to_ctrl_stop_bits;
+   
+   // Memory map to datapath signals
+   sc_signal<sc_uint<DATA_W>> mem_to_dp_data;
+   
+   // Datapath to memory map signals
+   sc_signal<sc_uint<DATA_W>> dp_to_mem_data;
+   sc_signal<sc_uint<ADDR_W>> dp_to_mem_addr;
+   sc_signal<bool> dp_to_mem_write_enable;
+   
+   // Internal signals for start and memory write enable
+   sc_signal<bool> start_signal;
+   sc_signal<bool> mem_we_signal;
+   
+   // Internal input values
+   bool in_rst;
+   sc_uint<DATA_W> in_data_in;
+   sc_uint<ADDR_W> in_addr;
+   bool in_chip_select;
+   bool in_read_write;
+   bool in_write_enable;
+   bool in_rx_in;
+   bool in_start;
+   bool in_mem_we;
+ 
+   // Top-level methods
+   void process();
+   void read_inputs();
+   void write_outputs();
+ 
+   // Test methods
+   bool test_reset_datapath();
+   bool test_reset_controller();
+   bool test_reset_memory_map();
+ 
+   SC_CTOR(top) : datapath_inst("datapath_inst"), 
+                 controller_inst("controller_inst"), 
+                 memory_map_inst("memory_map_inst") {
+     SC_THREAD(process);
+     sensitive << clk.pos();
+     async_reset_signal_is(rst, false);
+ 
+     // Connect all the Datapath Signals
+     datapath_inst.clk(clk);
+     datapath_inst.rst(rst);
+     datapath_inst.load_tx(ctrl_to_dp_load_tx);
+     datapath_inst.load_tx2(ctrl_to_dp_load_tx2);
+     datapath_inst.tx_start(ctrl_to_dp_tx_start);
+     datapath_inst.tx_data(ctrl_to_dp_tx_data);
+     datapath_inst.tx_parity(ctrl_to_dp_tx_parity);
+     datapath_inst.tx_stop(ctrl_to_dp_tx_stop);
+     datapath_inst.rx_start(ctrl_to_dp_rx_start);
+     datapath_inst.rx_data(ctrl_to_dp_rx_data);
+     datapath_inst.rx_parity(ctrl_to_dp_rx_parity);
+     datapath_inst.rx_stop(ctrl_to_dp_rx_stop);
+     datapath_inst.error_handle(ctrl_to_dp_error_handle);
+     datapath_inst.rx_read(ctrl_to_dp_rx_read);
+     datapath_inst.tx_buffer_full(dp_to_ctrl_tx_buffer_full);
+     datapath_inst.rx_buffer_empty(dp_to_ctrl_rx_buffer_empty);
+     datapath_inst.parity_error(dp_to_ctrl_parity_error);
+     datapath_inst.framing_error(dp_to_ctrl_framing_error);
+     datapath_inst.overrun_error(dp_to_ctrl_overrun_error);
+     datapath_inst.ctrl_parity_enabled(dp_to_ctrl_parity_enabled);
+     datapath_inst.ctrl_parity_even(dp_to_ctrl_parity_even);
+     datapath_inst.ctrl_data_bits(dp_to_ctrl_data_bits);
+     datapath_inst.ctrl_stop_bits(dp_to_ctrl_stop_bits);
+     datapath_inst.rx_in(rx_in);
+     datapath_inst.tx_out(tx_out);
+     datapath_inst.data_in(mem_to_dp_data);
+     datapath_inst.data_out(dp_to_mem_data);
+     datapath_inst.addr(dp_to_mem_addr);
+     datapath_inst.dp_data_in(dp_to_mem_data);
+     datapath_inst.dp_addr(dp_to_mem_addr);
+     datapath_inst.dp_write_enable(dp_to_mem_write_enable);
+     datapath_inst.start(start_signal);
+     datapath_inst.mem_we(mem_we_signal);
+     
+     // Connect all the Controller Signals
+     controller_inst.clk(clk);
+     controller_inst.rst(rst);
+     controller_inst.start(start_signal);
+     controller_inst.mem_we(mem_we_signal);
+     controller_inst.tx_buffer_full(dp_to_ctrl_tx_buffer_full);
+     controller_inst.rx_buffer_empty(dp_to_ctrl_rx_buffer_empty);
+     controller_inst.rx_in(rx_in);
+     controller_inst.parity_error(dp_to_ctrl_parity_error);
+     controller_inst.framing_error(dp_to_ctrl_framing_error);
+     controller_inst.overrun_error(dp_to_ctrl_overrun_error);
+     controller_inst.parity_enabled(dp_to_ctrl_parity_enabled);
+     controller_inst.parity_even(dp_to_ctrl_parity_even);
+     controller_inst.data_bits(dp_to_ctrl_data_bits);
+     controller_inst.stop_bits(dp_to_ctrl_stop_bits);
+     controller_inst.load_tx(ctrl_to_dp_load_tx);
+     controller_inst.load_tx2(ctrl_to_dp_load_tx2);
+     controller_inst.tx_start(ctrl_to_dp_tx_start);
+     controller_inst.tx_data(ctrl_to_dp_tx_data);
+     controller_inst.tx_parity(ctrl_to_dp_tx_parity);
+     controller_inst.tx_stop(ctrl_to_dp_tx_stop);
+     controller_inst.rx_start(ctrl_to_dp_rx_start);
+     controller_inst.rx_data(ctrl_to_dp_rx_data);
+     controller_inst.rx_parity(ctrl_to_dp_rx_parity);
+     controller_inst.rx_stop(ctrl_to_dp_rx_stop);
+     controller_inst.rx_read(ctrl_to_dp_rx_read);
+     controller_inst.error_handle(ctrl_to_dp_error_handle);
+     
+     // Connect all the Memory Map signals
+     memory_map_inst.clk(clk);
+     memory_map_inst.rst(rst);
+     memory_map_inst.data_in(data_in);
+     memory_map_inst.data_out(data_out);
+     memory_map_inst.addr(addr);
+     memory_map_inst.chip_select(chip_select);
+     memory_map_inst.read_write(read_write);
+     memory_map_inst.write_enable(write_enable);
+     memory_map_inst.dp_data_out(mem_to_dp_data);
+     memory_map_inst.dp_data_in(dp_to_mem_data);
+     memory_map_inst.dp_addr(dp_to_mem_addr);
+     memory_map_inst.dp_write_enable(dp_to_mem_write_enable);
+     memory_map_inst.tx_buffer_full(dp_to_ctrl_tx_buffer_full);
+     memory_map_inst.rx_buffer_empty(dp_to_ctrl_rx_buffer_empty);
+     memory_map_inst.error_indicator(error_indicator);
+   }
+   
+ #ifdef NC_SYSTEMC
+  public:
+   void ncsc_replace_names(){
+     // Replace all the names using ncsc_replace_name for inputs and outputs
+     
+     // Inputs
+     ncsc_replace_name(clk, "clk");                      // Port 0
+     ncsc_replace_name(rst, "rst");                      // Port 1
+     ncsc_replace_name(data_in, "data_in");              // Port 2
+     ncsc_replace_name(addr, "addr");                    // Port 3
+     ncsc_replace_name(chip_select, "chip_select");      // Port 4
+     ncsc_replace_name(read_write, "read_write");        // Port 5
+     ncsc_replace_name(write_enable, "write_enable");    // Port 6
+     ncsc_replace_name(rx_in, "rx_in");                  // Port 7
+     
+     // Outputs
+     ncsc_replace_name(data_out, "data_out");            // Port 8
+     ncsc_replace_name(tx_out, "tx_out");                // Port 9
+     ncsc_replace_name(tx_buffer_full, "tx_buffer_full");// Port 10
+     ncsc_replace_name(rx_buffer_empty, "rx_buffer_empty");// Port 11
+     ncsc_replace_name(error_indicator, "error_indicator");// Port 12
+   }
+ #endif
+ };
+ 
+ #endif
